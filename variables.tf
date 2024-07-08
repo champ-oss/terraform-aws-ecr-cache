9d26ad9bf19639195aa01ecda8d0924d25833502
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-08 17:13:05 -0500

    toggle module

--- variables.tf
+++ variables.tf
@@ -56,3 +56,9 @@ variable "force_delete" {
   type        = bool
   default     = true
 }
+
+variable "enabled" {
+  description = "Set to false to prevent the module from creating any resources"
+  type        = bool
+  default     = true
+}
